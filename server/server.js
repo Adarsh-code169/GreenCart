aef2a1902bfd6d4e54b83db87c362c5f346d1ca0
Adarsh Shukla <adarshvardhanshukla269@gmail.com>
2026-02-28 22:38:46 +0530

    Final commit 2

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL];
 
 app.post(
   '/stripe',
