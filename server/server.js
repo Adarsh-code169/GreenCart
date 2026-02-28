6ab84f0d7faa5390c79c6b94b7e0e86be8a2780b
Adarsh Shukla <adarshvardhanshukla269@gmail.com>
2026-03-01 00:34:14 +0530

    allowed cors from anywhere

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ app.post(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 
 app.get('/', (req, res) => res.send("API is Working"));
