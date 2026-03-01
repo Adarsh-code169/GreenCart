d7835ed1585132ad661d1312d12fe9ead9710ec9
Adarsh Shukla <adarshvardhanshukla269@gmail.com>
2026-03-01 17:47:34 +0530

    Final committ

--- server/server.js
+++ server/server.js
@@ -29,10 +29,7 @@ app.post(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "*",
-  credentials: true
-}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 
 app.get('/', (req, res) => res.send("API is Working"));
