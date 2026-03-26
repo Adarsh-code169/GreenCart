be25afcb608f75db69a5838027201531ae8f454f
Adarsh Shukla <adarshvardhanshukla269@gmail.com>
2026-03-26 22:41:37 +0530

    chore: Update allowed CORS origin for Vercel frontend deployment.

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://green-cart-topaz-beta.vercel.app"
+  "https://greencart-frontend-eta.vercel.app"
 ];
 
 app.use(cors({
